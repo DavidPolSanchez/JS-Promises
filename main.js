b53028d19f78406a27916ca54ad84791e8888a81
David Pol <davidpolsanchezmartos@gmail.com>
2023-06-05 20:06:19 +0200

    Add Arrays

--- main.js
+++ main.js
@@ -3,13 +3,8 @@ var altura="169";
 var concatenado= nombre + " " + altura;
 
 
-var datos= document.getElementById('datos');
 
- datos.innerHTML = `
-    <h1>Soy la caja de datos </h1>
-    <h2>mi nombre es ${nombre}</h2>
-    <h3>mido: ${altura} cm </h3>
-`;
+
 
 if(altura >=180){
     datos.innerHTML += '<h1>Eres una persona alta</h1>';
@@ -21,6 +16,33 @@ for(var i=2000;i<=2023;i++){
     datos.innerHTML +='<h2>estamos en el año '+i;
 }
 
+//functions
+function muestraMiNombre(nombre,altura){
+    var datos = `
+        <h1>Soy la caja de datos </h1>
+        <h2>mi nombre es ${nombre}</h2>
+        <h3>mido: ${altura} cm </h3>
+    `;
+    return datos;
+
+}
+function imprimir(){
+    var datos= document.getElementById('datos');
+    datos.innerHTML = muestraMiNombre("david",169);
+}
+
+imprimir("david",169);
+//Arrays
+
+var nombres =["david","Pol","Sanchez"]
+/*
+for(var i=0;i<nombres.length;i++){
+    
+    document.write(nombres[i]+'</br>')
+}*/
+nombres.forEach((nombre)=>{
+    document.write(nombre+'</br>')
+})
 coche.cogerDatos();
 console.log(coche);
 
